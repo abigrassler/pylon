e64f87fa41c0181dd979df215b635fd8a3cc7d19
spaderthomas <thomas.spader@gmail.com>
2025-06-01 16:50:56 -0400

    fix frame time

--- multi_camera.py
+++ multi_camera.py
@@ -101,51 +101,46 @@ class Context:
         frame_delta = grab.GetTimeStamp() - camera.frame_time
         max_frame_delta = self.max_frame_delta.total_seconds() * (10 ** 9) # Convert our delta from seconds to nanoseconds
 
-        print(f'{camera.name}, {camera.frame_time}, {grab.GetTimeStamp()}, {frame_delta}, {max_frame_delta}')
-
         if frame_delta > max_frame_delta:
-          print('too big')
+          print(f'Frame delta exceeded; delta = {frame_delta}, max delta = {max_frame_delta}; assuming beam status changed and starting a new video')
 
-        # if frame_delta > max_frame_delta:
-        #   print(f'Frame delta exceeded; delta = {frame_delta}, max delta = {max_frame_delta}; assuming beam status changed and starting a new video')
+          for camera in self.cameras.values():
+            # If this is our first video, there's no current video to finalize
+            if camera.video_writer:
+              print('Finishing previous video')
+              camera.video_writer.release()
 
-        #   for camera in self.cameras.values():
-        #     # If this is our first video, there's no current video to finalize
-        #     if camera.video_writer:
-        #       print('Finishing previous video')
-        #       camera.video_writer.release()
+              file_name = f'metadata_{camera.name}_{camera.video_timestamp}'
+              file_path = os.path.join(camera.output_directory, file_name)
+              df = pd.DataFrame(camera.metadata, columns=["Timestamp_ns", "LineStatusAll", "CounterValue"])
+              df.to_csv(file_path, index=False)
 
-        #       file_name = f'metadata_{camera.name}_{camera.video_timestamp}'
-        #       file_path = os.path.join(camera.output_directory, file_name)
-        #       df = pd.DataFrame(camera.metadata, columns=["Timestamp_ns", "LineStatusAll", "CounterValue"])
-        #       df.to_csv(file_path, index=False)
+            # Start a new video
+            camera.video_timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S-%f')
+            file_name = f"{camera.name}_{camera.video_timestamp}.avi"
+            file_path = os.path.join(camera.output_directory, file_name)
 
-        #     # Start a new video
-        #     camera.video_timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S-%f')
-        #     file_name = f"{camera.name}_{camera.video_timestamp}.avi"
-        #     file_path = os.path.join(camera.output_directory, file_name)
+            print(f'Starting new video for {camera.name} at {file_path}')
 
-        #     print(f'Starting new video for {camera.name} at {file_path}')
+            camera.video_writer = cv2.VideoWriter(
+              file_path,
+              self.fourcc,
+              self.frame_rate,
+              self.output_resolution
+            )
 
-        #     camera.video_writer = cv2.VideoWriter(
-        #       file_path,
-        #       self.fourcc,
-        #       self.frame_rate,
-        #       self.output_resolution
-        #     )
-
-        #     camera.metadata = []
+            camera.metadata = []
 
         camera.frame_time = grab.GetTimeStamp()
 
-        # frame = self.converter.Convert(grab).GetArray()
-        # camera.video_writer.write(frame)
+        frame = self.converter.Convert(grab).GetArray()
+        camera.video_writer.write(frame)
 
-        # camera.metadata.append((
-        #   grab.ChunkTimestamp.Value,
-        #   grab.ChunkLineStatusAll.Value,
-        #   grab.ChunkCounterValue.Value
-        # ))
+        camera.metadata.append((
+          grab.ChunkTimestamp.Value,
+          grab.ChunkLineStatusAll.Value,
+          grab.ChunkCounterValue.Value
+        ))
 
     except KeyboardInterrupt:
        print('Recording was stopped by user.')
