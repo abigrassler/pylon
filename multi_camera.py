36f861889bf5665a8aeddff676ad78d19b824fbf
spaderthomas <thomas.spader@gmail.com>
2025-06-01 16:43:35 -0400

    fix frame time

--- multi_camera.py
+++ multi_camera.py
@@ -13,6 +13,7 @@ class Camera:
   def __init__(self, name):
     self.name = name
     self.output_directory = os.path.join('D', os.path.sep, 'abi_data', 'raw_data', 'setup', 'test_cameras', self.name)
+    self.frame_time = 0
     self.metadata = []
     self.video_timestamp = None
 
@@ -36,7 +37,6 @@ class Context:
 
     # The cameras are triggered by an Arduino; when we don't get triggered within a certain amount of time, we assume the beam
     # has become unbroken
-    self.frame_timestamp = 0
     self.max_frame_delta = timedelta(seconds=self.sampling_rate * 1.5)
 
     # Create an image format converter. This is used to convert the raw frames to something that can be written to a video
@@ -98,51 +98,49 @@ class Context:
         camera_id = grab.GetCameraContext()
         camera = self.cameras[camera_id]
 
-        frame_delta = grab.GetTimeStamp() - self.frame_timestamp
+        frame_delta = grab.GetTimeStamp() - camera.frame_time
         max_frame_delta = self.max_frame_delta.total_seconds() * (10 ** 9) # Convert our delta from seconds to nanoseconds
 
-        print(f'{camera.name}, {frame_delta / 1000000.0}')
+        if frame_delta > max_frame_delta:
+          print(f'Frame delta exceeded; delta = {frame_delta}, max delta = {max_frame_delta}; assuming beam status changed and starting a new video')
 
-        # if frame_delta > max_frame_delta:
-        #   print(f'Frame delta exceeded; delta = {frame_delta}, max delta = {max_frame_delta}; assuming beam status changed and starting a new video')
+          for camera in self.cameras.values():
+            # If this is our first video, there's no current video to finalize
+            if camera.video_writer:
+              print('Finishing previous video')
+              camera.video_writer.release()
 
-        #   for camera in self.cameras.values():
-        #     # If this is our first video, there's no current video to finalize
-        #     if camera.video_writer:
-        #       print('Finishing previous video')
-        #       camera.video_writer.release()
+              file_name = f'metadata_{camera.name}_{camera.video_timestamp}'
+              file_path = os.path.join(camera.output_directory, file_name)
+              df = pd.DataFrame(camera.metadata, columns=["Timestamp_ns", "LineStatusAll", "CounterValue"])
+              df.to_csv(file_path, index=False)
 
-        #       file_name = f'metadata_{camera.name}_{camera.video_timestamp}'
-        #       file_path = os.path.join(camera.output_directory, file_name)
-        #       df = pd.DataFrame(camera.metadata, columns=["Timestamp_ns", "LineStatusAll", "CounterValue"])
-        #       df.to_csv(file_path, index=False)
+            # Start a new video
+            camera.video_timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S-%f')
+            file_name = f"{camera.name}_{camera.video_timestamp}.avi"
+            file_path = os.path.join(camera.output_directory, file_name)
 
-        #     # Start a new video
-        #     camera.video_timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S-%f')
-        #     file_name = f"{camera.name}_{camera.video_timestamp}.avi"
-        #     file_path = os.path.join(camera.output_directory, file_name)
+            print(f'Starting new video for {camera.name} at {file_path}')
 
-        #     print(f'Starting new video for {camera.name} at {file_path}')
+            camera.video_writer = cv2.VideoWriter(
+              file_path,
+              self.fourcc,
+              self.frame_rate,
+              self.output_resolution
+            )
 
-        #     camera.video_writer = cv2.VideoWriter(
-        #       file_path,
-        #       self.fourcc,
-        #       self.frame_rate,
-        #       self.output_resolution
-        #     )
+            camera.metadata = []
 
-        #     camera.metadata = []
+        camera.frame_time = grab.GetTimeStamp()
 
-        # self.frame_timestamp = grab.GetTimeStamp()
+        frame = self.converter.Convert(grab).GetArray()
+        camera.video_writer.write(frame)
 
-        # frame = self.converter.Convert(grab).GetArray()
-        # camera.video_writer.write(frame)
-
-        # camera.metadata.append((
-        #   grab.ChunkTimestamp.Value,
-        #   grab.ChunkLineStatusAll.Value,
-        #   grab.ChunkCounterValue.Value
-        # ))
+        camera.metadata.append((
+          grab.ChunkTimestamp.Value,
+          grab.ChunkLineStatusAll.Value,
+          grab.ChunkCounterValue.Value
+        ))
 
     except KeyboardInterrupt:
        print('Recording was stopped by user.')
